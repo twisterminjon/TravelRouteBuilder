1ae0716db346ea582a8ba78b42e754df5736a9e9
Alexandr Navarich <twister.minjon@gmail.com>
2025-08-12 16:11:35 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      <h1>TravelRouteBuilder</h1>
+      <p>React + TypeScript + ReactFlow project</p>
+    </div>
+  )
+}
+
+export default App
